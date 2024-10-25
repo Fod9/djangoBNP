c8d804cdfdfbdd24708b6e285a85c6239cae5891
Spokay <hugobressange@gmail.com>
2024-10-25 15:30:57 +0200

    scheduler started with the app

--- banquemanager/wsgi.py
+++ banquemanager/wsgi.py
@@ -8,9 +8,22 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import threading
 
 from django.core.wsgi import get_wsgi_application
+
+from banquemanager.management.commands.start_scheduler import start_scheduler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banquemanager.settings')
 
 application = get_wsgi_application()
+
+if os.environ.get('RUN_MAIN') == 'true':
+    from django.core.management import call_command
+
+    def start_scheduler():
+        call_command('start_scheduler')
+
+
+    # Lancer le scheduler dans un thread séparé
+    threading.Thread(target=start_scheduler).start()
